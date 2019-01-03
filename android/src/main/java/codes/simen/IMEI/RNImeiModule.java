687bdbdcb01e3cfbc95fa5c084a6ff58ce720bdc
César Campos <cesar.campos@boosmap.com>
2019-01-02 21:34:18 -0300

    corrected check on permissions

--- android/src/main/java/codes/simen/IMEI/RNImeiModule.java
+++ android/src/main/java/codes/simen/IMEI/RNImeiModule.java
@@ -2,6 +2,7 @@ package codes.simen.IMEI;
 
 import android.content.Context;
 import android.telephony.TelephonyManager;
+import android.content.pm.PackageManager;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
@@ -27,18 +28,26 @@ public class RNImeiModule extends ReactContextBaseJavaModule {
     public Map<String, Object> getConstants() {
         final Map<String, Object> constants = new HashMap<>();
 
-        TelephonyManager tm = (TelephonyManager) this.reactContext.getSystemService(Context.TELEPHONY_SERVICE);
-        String imei="";
-        if (android.os.Build.VERSION.SDK_INT >= 26) {
-            imei = tm.getImei().trim();
-        } else {
-            imei= tm.getDeviceId();
+        String IMEINumber = "444444444444444";
+        String permission = "android.permission.READ_PHONE_STATE";
+        int res = this.reactContext.checkCallingOrSelfPermission(permission);
+
+        if (res == PackageManager.PERMISSION_GRANTED) {
+            try {
+                TelephonyManager tm = (TelephonyManager) this.reactContext.getSystemService(Context.TELEPHONY_SERVICE);
+                if (android.os.Build.VERSION.SDK_INT >= 26) {
+                    IMEINumber = tm.getImei().trim();
+                } else {
+                    IMEINumber= tm.getDeviceId();
+                }
+            }
+            catch (Exception e) {
+                IMEINumber = "555555555555555";
+                e.printStackTrace();
+            }
         }
 
-        if (imei.isEmpty()) {
-            throw new RuntimeException("Failed to read IMEI (imei is empty!)");
-        }
-        constants.put("imei", imei);
+        constants.put("imei", IMEINumber);
 
         return constants;
     }
